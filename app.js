9eb0580087ec88b1907cac24b8771eb59345d774
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-25 00:16:34 -0500

    ui and server modified

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+var app = require('express')();
+var express = require('express');
+var cfenv = require('cfenv');
+var cors = require('cors');
+var app = express();
+var bodyParser = require('body-parser');
 var paypal = require('paypal-rest-sdk');
+var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
+var authToken = "650d049a9bd99323fb899ce4b9e84fcc";
+var client = require('twilio')(accountSid, authToken);
+app.use(cors());
+app.use(bodyParser.json());
+
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
   'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
   'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
 });
-
+app.listen(1337, '127.0.0.1', function() {
+    console.log("server starting on " + 1337);
+});
 
 
 
@@ -44,17 +58,17 @@ var create_payment_json = {
         "description": "This is the payment transaction description."
     }]
 };
-//console.log('here');
-
-paypal.payment.create(create_payment_json, function (error, payment) {
-    if (error) {
-        throw error;
-    } else {
-        //console.log("Create Payment Response");
-        //console.log(payment);
-    }
+var flag=false;
+app.get('/createPayment',function(req,res){
+    paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+            throw error;
+        } else {
+            flag=true;
+            res.end();
+        }
+    });
 });
-
 
 var search_attr = {
     "start_invoice_date": "2010-05-10 PST",
@@ -64,24 +78,29 @@ var search_attr = {
     "total_count_required": true
 };
 
-paypal.invoice.search(search_attr, function (error, results) {
-    if (error) {
-        throw error;
-    } else {
-        //console.log("Invoice Search Response");
-        //console.log(results);
-    }
+app.get('/checkPayment',function(req,res){
+    paypal.invoice.search(search_attr, function (error, results) {
+        if (error) {
+            throw error;
+        } else {
+            if(flag){
+                flag=false;
+                res.end('1');
+            }
+            else
+                res.end('0');
+        }
+    });
 });
 
-var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
-var authToken = "650d049a9bd99323fb899ce4b9e84fcc";
-var client = require('twilio')(accountSid, authToken);
-client.messages.create({
-    body: "Alarm",
-    to: "+16692269411",
-    from: "+14694164117"
-}, function(err, message) {
-    //console.log(err);
-    console.log(message);
-     
+app.get("/sendSms", function(req, res) {
+    console.log(req.query.number);
+    client.messages.create({
+        body: "Your Bill is ",
+        to: "+1"+req.query.number,
+        from: "+14694164117"
+    }, function(err, message) {
+        console.log(message);
+        res.end();
+    });
 });
--- js/angular-login.js
+++ js/angular-login.js
@@ -1,9 +1,47 @@
 var app=angular.module('myApp',[]);
 app.controller('myCtrl',function($scope,$http) {
-    $scope.openModal=function(){
-        $('#myModal').modal('show');
-        swal("Good job!", "You clicked the button!", "success");
-    }
+	$scope.gifyhide=false;
+	$scope.openModal=function(){
+		$('#myModal').modal('show');
+	}
+	$scope.phoneNumber='';
+	var sent=false;
+	$scope.sendSms=function(){
+		var phoneno = /^\d{10}$/;  
+		if($scope.phoneNumber.match(phoneno))  
+		{  
+			$http.get('http://localhost:1337/sendSms?number='+$scope.phoneNumber)
+			.success(function(req,res){
+				alertify.success('SMS Sent Successfully');
+				sent=true;
+				$scope.gifyhide=true;
+			});
+		}  
+		else  
+		{  
+			alertify.error('Invalid Phone Number');
+		}  
+	}
+	var count=0;
+
+	setInterval(function(){ 
+		if(sent){
+			$http.get('http://localhost:1337/checkPayment')
+			.success(function(req,res){
+				console.log(res);
+				count++;
+				if(count==10){
+					if(res=='1')
+						swal("Good job!", "Your payment is successful!", "success");
+					else
+						swal("Oops!", "You payment failed!", "error");
+					sent=false;
+					$scope.gifyhide=false;
+				}
+			})
+		}
+	}, 1000);
+
 
 
 
